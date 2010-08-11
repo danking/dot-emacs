62c713eff4b6e19645a8b20afcf7b6bb3bfe8626
Daniel King <danking@ccs.neu.edu>
2010-08-11 09:36:52 -0700

    clenaing up org-setup.el, moving notes file

--- org-setup.el
+++ org-setup.el
@@ -1,3 +1,4 @@
+(add-to-list 'load-path "~/.emacs.d/org-mode/lisp/")
 (require 'org-install)
 
 ;; The following lines are always needed.  Choose your own keys.
@@ -11,10 +12,8 @@
             (local-set-key (kbd "M-n") 'org-metaright)
             (local-set-key (kbd "M-p") 'org-metaleft)))
 
-(setq org-default-notes-file (concat org-directory "/.notes.org"))
+(setq org-default-notes-file (concat org-directory "/notes.org"))
 (global-set-key (kbd "C-c c") 'org-capture)
-
-(add-to-list 'load-path "~/.emacs.d/org-mode/lisp")
 
 ;; capture templates
 ;; (defun music-remember ()
