72ebdd272569a109fad7228eb075cf00dbd841ab
Daniel King <danking@ccs.neu.edu>
2010-08-11 11:00:34 -0700

    added capture templates

--- org-setup.el
+++ org-setup.el
@@ -1,57 +1,35 @@
 (add-to-list 'load-path "~/.emacs.d/org-mode/lisp/")
+
 (require 'org-install)
 (require 'org)
-;; The following lines are always needed.  Choose your own keys.
+
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+
+;; key bindings
 (global-set-key (kbd "C-c l") 'org-store-link)
 (global-set-key (kbd "C-c a") 'org-agenda)
 (global-set-key (kbd "C-c b") 'org-iswitchb)
-;(global-set-key (kbd "C-c r") 'org-remember)
+(global-set-key (kbd "C-c c") 'org-capture)
 (add-hook 'org-mode-hook
           (lambda ()
+            ;; change outline item depth
             (local-set-key (kbd "M-n") 'org-metaright)
             (local-set-key (kbd "M-p") 'org-metaleft)))
 
 (setq org-default-notes-file (concat org-directory "/notes.org"))
-(global-set-key (kbd "C-c c") 'org-capture)
 
 ;; capture templates
-;; (defun music-remember ()
-;;   (interactive)
-;;   (let* ((artist (read "Which artist? "))
-;;          (album  (read "What album? ")))
-;;     )
-;;   (let* ((myname (completing-read "Who is calling? "
-;;                                   (bbdb-hashtable)
-;;                                   'bbdb-completion-predicate
-;;                                   'confirm))
-;;          (my-bbdb-name (if (> (length myname) 0) myname nil)))
-;;     (setq gjg/capture-phone-record
-;;           (if my-bbdb-name
-;;               (first (or (bbdb-search (bbdb-records) my-bbdb-name nil nil)
-;;                          (bbdb-search (bbdb-records) nil my-bbdb-name nil)))
-;;             myname))
-;;     (other-window 1)
-;;     (let ((org-capture-templates '(("P"
-;;                                     "Phone"
-;;                                     entry
-;;                                     (file "~/git/org/refile.org")
-;;                                     :clock-in t :clock-resume t))))
-;;       (org-capture))))
-
-;; (defun gjg/bbdb-name ()
-;;   "Return full name of saved bbdb record, or empty string - for use in Capture templates"
-;;   (if (and gjg/capture-phone-record (vectorp gjg/capture-phone-record))
-;;       (concat "[[bbdb:"
-;;               (bbdb-record-name gjg/capture-phone-record) "]["
-;;               (bbdb-record-name gjg/capture-phone-record) "]]")
-;;     "NAME"))
-
-;; (defun gjg/bbdb-company ()
-;;   "Return company of saved bbdb record, or empty string - for use in Capture templates"
-;;   (if (and gjg/capture-phone-record (vectorp gjg/capture-phone-record))
-;;       (or (bbdb-record-company gjg/capture-phone-record) "")
-;;     "COMPANY"))
-
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline "~/org/notes.org" "Tasks")
+         "* TODO %?\n %t")
+        ("n" "Note" entry (file+headline "~/org/notes.org" "Tasks")
+         "* TODO %?\n  %i\n  %a")
+        ("j" "Journal" entry (file+datetree "~/org/journal.org")
+         "* %?\nEntered on %U\n  %i\n")
+        ("m" "Music" entry (file+headline "~/org/tobuy.org" "Music")
+         "* %^{Album} by %^{Artist}")
+        ("b" "Books/Reading Material" entry (file+headline "~/org/tobuy.org"
+                                                           "Books")
+         "* %^{Title} by %^{Author}")))
 
 (provide 'org-setup)
