d18e7cb47b6798258f57b4fa515b2c7c00d3d9db
Daniel King <danking@ccs.neu.edu>
2011-01-22 01:26:08 -0500

    added an epic mingus buffer switching key binding

--- apps.el
+++ apps.el
@@ -1,4 +1,13 @@
 (add-to-list 'load-path "~/emacs.d/mingus/")
 (autoload 'mingus "mingus-stays-home")
 
+;; fucking 3pic switcheroo
+(global-set-key (kbd "C-c m")
+                (lexical-let ((previous-buffer (current-buffer)))
+                  (lambda ()
+                    (interactive)
+                    (if (string-match "\*Mingus.*" (buffer-name))
+                        (switch-to-buffer previous-buffer)
+                        (mingus)))))
+
 (provide 'apps)
