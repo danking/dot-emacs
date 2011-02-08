d071665ebc0393adc18bd884c1cb32d3f65e0eee
Alex Lee <lee@ccs.neu.edu>
2011-02-07 20:14:12 -0500

    add a function to compile all el files in .emacs.d

--- emacs-utilities.el
+++ emacs-utilities.el
@@ -1,4 +1,13 @@
 (require 'unbound)
 (message "starting Emacs server for emacsclient")
 (server-start)
+
+(defun recompile-emacs-lisp-files-in-emacs.d
+  "recompiles all the emacs lisp files in emacs.d"
+  (interactive)
+  (mapcar (lambda (dir)
+            (when (string-match ".*\.el$" dir)
+              (byte-compile-file dir)))
+          (directory-files "~/.emacs.d/")))
+
 (provide 'emacs-utilities)
