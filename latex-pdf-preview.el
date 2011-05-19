56e4d54a4e1d31e04c75bb9de6c0f7b227692b75
Daniel King <danking@ccs.neu.edu>
2011-05-19 14:16:17 -0400

    created `latex-recompile-references' for use whenever I want to recompile bibtex references before I preview the paper

--- latex-pdf-preview.el
+++ latex-pdf-preview.el
@@ -1,6 +1,6 @@
 (defun latex-pdf-preview ()
   (interactive)
-  (message buffer-file-name)
+  (message "producing a new pdf")
   (call-process "pdflatex"
                 nil
                 "*pdflatex-output*"
@@ -11,8 +11,23 @@
                                                    buffer-file-name)))
     (find-file-other-window output-filename)))
 
+(defun latex-recompile-references ()
+  (interactive)
+  (message "recompiling the references and producing a new pdf")
+  (let* ((document-name (replace-regexp-in-string "\\.tex$" "" buffer-file-name))
+         (run (lambda (x)
+                (call-process x nil "*pdflatex-output*" nil document-name))))
+    (funcall run "pdflatex")   ; generate aux file
+    (funcall run "bibtex")     ; generate bibliography
+    (funcall run "pdflatex")   ; reads bibliography and sets labels in aux
+    (latex-pdf-preview))       ; finally generates a complete pdf and shows it
+  (message "done"))
+
 (add-hook 'latex-mode-hook (lambda ()
                              (local-set-key (kbd "C-c p")
                                             'latex-pdf-preview)))
+(add-hook 'latex-mode-hook (lambda ()
+                             (local-set-key (kbd "C-c b")
+                                            'latex-recompile-references)))
 
 (provide 'latex-pdf-preview)
