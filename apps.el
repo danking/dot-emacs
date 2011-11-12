a5e474500e0d7f1c5cde812214d520acfe2879c7
Daniel King <danking@ccs.neu.edu>
2011-11-12 13:22:47 -0500

    Added documentation string for the mingus switching command "C-c m"

--- apps.el
+++ apps.el
@@ -3,15 +3,16 @@
 
 (require 'cl) ;; for lexical-let
 ;; fucking 3pic switcheroo
-(global-set-key (kbd "C-c m")
-                (lexical-let ((previous-buffer (current-buffer)))
-                  (lambda ()
-                    (interactive)
-                    (if (string-match "\*Mingus.*" (buffer-name))
-                        (switch-to-buffer previous-buffer)
-                        (progn
-                          (setq previous-buffer (current-buffer))
-                          (mingus))))))
+(lexical-let ((previous-buffer (current-buffer)))
+  (defun mingus-switch-in/out ()
+    "Switches between a mingus buffer and the buffer which originally called this procedure."
+    (interactive)
+    (if (string-match "\*Mingus.*" (buffer-name))
+        (switch-to-buffer previous-buffer)
+      (progn
+        (setq previous-buffer (current-buffer))
+        (mingus)))))
+(global-set-key (kbd "C-c m") 'mingus-switch-in/out)
 
 (add-to-list 'load-path "~/.emacs.d/gist.el")
 (require 'gist)
