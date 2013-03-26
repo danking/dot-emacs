6fc432edc8612c702d3a0ba0f65c25c82f47e8b8
Daniel King <danking@ccs.neu.edu>
2013-03-25 22:02:23 -0400

    Add eim-py, a smart pinyin input mode

--- interface.el
+++ interface.el
@@ -99,4 +99,13 @@
 (setq browse-url-browser-function 'browse-url-generic
       browse-url-generic-program "google-chrome")
 
+(add-to-list 'load-path "~/.emacs.d/site-lisp/eim")
+(autoload 'eim-use-package "eim" "Another emacs input method")
+(register-input-method
+ "eim-wb" "euc-cn" 'eim-use-package
+ "五笔" "汉字五笔输入法" "~/.emacs.d/site-lisp/eim/wb.txt")
+(register-input-method
+ "eim-py" "euc-cn" 'eim-use-package
+ "拼音" "汉字拼音输入法" "~/.emacs.d/site-lisp/eim/py.txt")
+
 (provide 'interface)
