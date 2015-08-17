0a878001467c01589ed6eb50a28774de63df7a02
Daniel King <daniel.king@trinetx.com>
2015-08-17 13:47:58 -0400

    feature: enable magit github pull requests

--- extra-elisp/coding-utilities.el
+++ extra-elisp/coding-utilities.el
@@ -1,4 +1,8 @@
 (require 'magit)
 (global-set-key (kbd "C-x g") 'magit-status)
 
+(require 'magit-gh-pulls)
+(add-hook 'magit-mode-hook 'turn-on-magit-gh-pulls)
+
 (provide 'coding-utilities)
+;;; coding-utilities.el ends here
