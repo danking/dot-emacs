8da219d17e9ce4ad18d4bf401f395acb52c142df
Daniel King <daniel.king@trinetx.com>
2015-08-17 13:48:12 -0400

    feature: C-l actually clears the eshell

--- extra-elisp/shellfixes.el
+++ extra-elisp/shellfixes.el
@@ -36,4 +36,14 @@
    )
   )
 
+(defun eshell-clear-buffer ()
+  "Clear terminal"
+  (interactive)
+  (let ((inhibit-read-only t))
+    (erase-buffer)
+    (eshell-send-input)))
+(add-hook 'eshell-mode-hook
+      '(lambda()
+          (local-set-key (kbd "C-l") 'eshell-clear-buffer)))
+
 (provide 'shellfixes)
